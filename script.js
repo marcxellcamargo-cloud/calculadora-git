588fb9ee126927ca1cb34f02bb406b7902e6400f
Marcelo Siqueira <marcxellcamargo@gmail.com>
2025-11-10 22:49:48 -0300

    Removendo botão de apagar

--- script.js
+++ script.js
@@ -71,11 +71,6 @@ document.addEventListener('keydown', function (event) {
         calcular();
     } else if (tecla === 'Escape') {
         limpar();
-        function apagar() {
-            numeroAtual = numeroAtual.slice(0, -1);
-            atualizarDisplay();
-        }
-
     } else if (tecla === '%') {
         porcentagem();
     }
